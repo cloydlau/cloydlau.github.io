59a27798d7f7aec978496561e1793dcfa003eefe
Cloyd Lau <31238760+cloydlau@users.noreply.github.com>
2024-10-12 20:39:56 +0800

    feat(json-editor-vue): add attw

--- playground/json-editor-vue/getStatistics.js
+++ playground/json-editor-vue/getStatistics.js
@@ -1,7 +1,26 @@
 const name = 'json-editor-vue'
-const initialPublishDate = '2020-07-15'
+// const initialPublishDate = '2020-07-15'
 const date = new Date()
-const currentDate = date.toISOString().split('T')[0]
+
+function formateDate(date) {
+  return date.toISOString().split('T')[0]
+}
+
+function getLastMonthToday(date) {
+  const today = date.getDate()
+  // 设置日期为上个月同一天，但如果无效则自动调整
+  date.setMonth(date.getMonth() - 1)
+
+  // 如果设置的日期结果在未来，则说明上个月没有对应的今天（例如31号）
+  if (date.getDate() !== today) {
+    date.setDate(0) // 设置为上个月的最后一天
+  }
+
+  return date
+}
+
+const currentDate = formateDate(date)
+const lastMonthToday = formateDate(getLastMonthToday(date))
 
 async function fetchWithCache(...args) {
   const [url] = args
@@ -35,24 +54,24 @@ function npmDownloads() {
   // 总量，但存在跨域限制：
   // `https://npm-stat.com/api/download-counts?package=json-editor-vue&from=${initialPublishDate}&until=${currentDate}`
   // 最大支持18个月：
-  return fetchWithCache(`https://api.npmjs.org/downloads/range/${initialPublishDate}:${currentDate}/${name}`).then((data) => {
-    const res = data.downloads.reduce((acc, day) => acc + day.downloads, 0)
-    console.log(`Total npm downloads: ${res}`)
-    return res
+  return fetchWithCache(`https://api.npmjs.org/downloads/range/${lastMonthToday}:${currentDate}/${name}`).then((data) => {
+    const res = data.downloads.reduce((acc, day) => acc + day.downloads, 0).toLocaleString()
+    console.log(`npm downloads: ${res}/month`)
+    return `${res}/month`
   })
 }
 
 function jsDelivrDownloads() {
   return fetchWithCache(`https://data.jsdelivr.com/v1/stats/packages/npm/${name}?period=all`).then((data) => {
     console.log(`Total jsDelivr downloads: ${data.hits.total}`)
-    return data.hits.total
+    return data.hits.total.toLocaleString()
   })
 }
 
 function githubStars() {
   return fetchWithCache(`https://api.github.com/repos/cloydlau/${name}`).then((data) => {
     console.log(`Total GitHub Stars: ${data.stargazers_count}`)
-    return data.stargazers_count ? `${data.stargazers_count} 🆘` : '🆘'
+    return data.stargazers_count ? `${data.stargazers_count.toLocaleString()} 🆘` : '🆘'
   })
 }
 
